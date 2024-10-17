17a4ad412ff4965d3f7e8bbd7bcf424b4b4c4217
Kent Tuneberg <ktunebe@gmail.com>
2024-10-16 23:20:34 -0500

    forked repo and started components

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input({
+    required: true
+  })
+  course: Course;
+}
